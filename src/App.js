f5f4f37aac741be81a56a3e4a89bf0cba7c66531
zarxy <zarxymen@gmail.com>
2023-09-06 13:12:53 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [validationError, setValidationError] = useState(false);
+   const [input, setInput] = useState("");
+   const [jsonData, setJsonData] = useState();
+   const [isLoading, seIsLoading] = useState();
+
+   const onSubmit = () => {
+      const regex = /^[0-9\.]{8,16}$/;
+
+      if (!regex.test(input)) {
+         setValidationError(true);
+         setJsonData();
+         return;
+      }
+      setValidationError(false);
+
+      seIsLoading(true);
+      fetch("https://api.api-ninjas.com/v1/iplookup?address=" + input, {
+         headers: { "X-Api-Key": "6QpmckgvhLRCWLPGjynYfIi4eGawIrBeCROxcxyI" }
+      })
+         .then((response) => response.json())
+         .then((data) => {
+            setJsonData(data);
+            console.log(data);
+         })
+         .catch((error) => {
+            console.error("Error:", error);
+            setJsonData(`${error}`);
+         })
+         .finally(() => seIsLoading(false));
+   };
+
+   const handleChange = (event) => {
+      setInput(event.target.value);
+   };
+
+   return (
+      <div className="App">
+         <div className="leftside">
+            <div className="container">
+               <h4>
+                  The IP check provides location information for any valid IP address.
+                  It works with both IPv4 and IPv6 addresses.
+               </h4>
+               <div className="Input">
+                  <input type="ip" onChange={handleChange} />
+                  <button type="button" onClick={onSubmit}>
+                     Check ip
+                  </button>
+               </div>
+               <div hidden={!validationError} className="validation-hint">
+                  <ul>
+                     <li>Contain numbers and dot only</li>
+                     <li>Minimum length 8</li>
+                     <li>Maximum length 16</li>
+                  </ul>
+               </div>
+            </div>
+         </div>
+         <div className="rightside">
+            <pre>
+               {isLoading ? "Loading..." : JSON.stringify(jsonData, null, 2)}
+            </pre>
+         </div>
+      </div>
+   );
 }
 
 export default App;
